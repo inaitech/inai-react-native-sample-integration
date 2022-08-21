192ffc44850a57fe7148a250b379f523446286ae
Amit Yadav <amit.y@inai.io>
2022-08-22 00:35:30 +0530

    Added ValidateFields method updated build settings and included missed inai android sdk

--- android/app/src/main/java/com/inai_react_native_sample_integration/InaiCheckoutModule.kt
+++ android/app/src/main/java/com/inai_react_native_sample_integration/InaiCheckoutModule.kt
@@ -3,11 +3,10 @@ package com.inai_react_native_sample_integration
 import android.util.Log
 import com.facebook.react.bridge.*
 import io.inai.android_sdk.*
-import org.json.JSONArray
 import org.json.JSONObject
 
 class InaiCheckoutModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext),
-    InaiCheckoutDelegate {
+    InaiCheckoutDelegate, InaiValidateFieldsDelegate {
 
     private var paymentCallback: Promise? = null
 
@@ -50,8 +49,43 @@ class InaiCheckoutModule(reactContext: ReactApplicationContext) : ReactContextBa
         }
     }
 
+    @ReactMethod
+    fun validateFields(
+        inaiToken: String,
+        orderId: String,
+        countryCode: String,
+        paymentMethodOption: String,
+        paymentDetailsObject: ReadableMap,
+        promise: Promise
+    ) {
+        this.paymentCallback = promise
+        val styles = InaiConfigStyles(
+            container = InaiConfigStylesContainer(backgroundColor = "#efefef"),
+            cta = InaiConfigStylesCta(backgroundColor = "#53509a"),
+            errorText = InaiConfigStylesErrorText(color = "#000000")
+        )
+
+        val config = InaiConfig(
+            token = inaiToken,
+            orderId = orderId,
+            styles = styles,
+            countryCode = countryCode,
+        )
+        try {
+            val checkout = InaiCheckout(config)
+            val paymentDetailsJSON = JSONObject(paymentDetailsObject.toString())
+            currentActivity.let {
+                if (it != null) {
+                    checkout.validateFields(paymentMethodOption, paymentDetailsJSON, it, this)
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("er", "Exception: $e")
+        }
+    }
+
     override fun paymentFinished(result: InaiPaymentResult) {
-        var resultData = JSONObject()
+        val resultData = JSONObject()
         var status = "canceled"
         if (result.status == InaiPaymentStatus.Success){
             status = "success"
@@ -64,9 +98,21 @@ class InaiCheckoutModule(reactContext: ReactApplicationContext) : ReactContextBa
         val jsonMap = convertJSONToWritableMap(resultData)
         paymentCallback?.resolve(jsonMap)
     }
+    override fun fieldsValidationFinished(result: InaiValidateFieldsResult) {
+        val resultData = JSONObject()
+        var status = "failed"
+        if (result.status == InaiValidateFieldsStatus.Success){
+            status = "success"
+        }
+        resultData.put("status", status)
+        resultData.put("data", result.data)
+
+        val jsonMap = convertJSONToWritableMap(resultData)
+        paymentCallback?.resolve(jsonMap)
+    }
 
     private fun convertJSONToWritableMap(jsonObject: JSONObject): WritableMap {
-        var wMap = WritableNativeMap()
+        val wMap = WritableNativeMap()
         val keys = jsonObject.keys()
         while(keys.hasNext()) {
             val key = keys.next()
