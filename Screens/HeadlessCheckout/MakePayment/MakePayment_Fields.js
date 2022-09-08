911dfbb2121ebb1a718990ec744dddef8f8fb7e0
Amit Yadav <amit.y@inai.io>
2022-09-08 12:32:49 +0530

    removed debug code

--- Screens/HeadlessCheckout/MakePayment/MakePayment_Fields.js
+++ Screens/HeadlessCheckout/MakePayment/MakePayment_Fields.js
@@ -65,9 +65,7 @@ const MakePayment_Fields = ({ navigation, route }) => {
   const submitPayment = () => {
     let fields = [];
     for (let f in paymentDetails) {
-      if (f != "number") {
-        fields.push({ "name": f, value: paymentDetails[f] });
-      }
+      fields.push({ "name": f, value: paymentDetails[f] });
     }
     console.log(fields);
     let paymentDetailsFields = { "fields": fields };
